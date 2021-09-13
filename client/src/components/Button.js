db01d21217976f17f8007dcf88640077b286d624
Jeehae Moon <jeehae.moon@gmail.com>
2021-09-13 10:22:43 -0400

    update2

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 const Button = ({ handleClick, type }) => {
   return (
-    <button onClick={handleClick} className={type}>
+    <ButtonDiv onClick={handleClick} className={type}>
       {type === "submit" ? "Subscribe" : "Reset"}
-    </button>
+    </ButtonDiv>
   );
 };
+
+const ButtonDiv = styled.button`
+  border-radius: 3px;
+  min-width: fit-content;
+  height: fit-content;
+  margin-right: 20px;
+  padding: 4%;
+  font-size: large;
+
+  @media (max-width: 1024px) {
+    font-size: 14px;
+  }
+`;
 
 export default Button;
